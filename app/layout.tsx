2aa61a63a9ca9f4c6ba589389a7ef4e2b444f054
Praharsha77 <116475816+Praharsha77@users.noreply.github.com>
2025-07-15 21:43:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Vercel Meet',
+  title: 'Chatuu 🔥',
   description: 'A simple video calling app',
   generator: 'Next.js',
 }
